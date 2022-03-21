36e1a330f485bfd3fc45ab4302fa2f17fa6b26d3
yu shu Lin <ben111497@gmail.com>
2022-03-21 14:04:31 +0800

    feat: add animation cancel & connect function
    - 解決動畫無法停止問題
    - 修改連線相關的方法

--- index.js
+++ index.js
@@ -5,24 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 var room = "no1"
-var userGroup = []
-var joinRoom = []
 var currentQuestionIndex = -1
 var timeoutID = setTimeout(timeOut, 1)
+var userGroup = []
+var joinRoom = []
 var finishID = []
-var isGameLoadingFinish = false
+var isReady = []
 
 /**
  * 路由
  */
-app.get('/a', (req, res) => {
-    res.sendFile(__dirname + '/clientA.html');
-});
-
-app.get('/b', (req, res) => {
-    res.sendFile(__dirname + '/clientB.html');
-});
-
 app.get('/', (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.end("hello worlds");
@@ -32,35 +24,46 @@ app.get('/', (req, res) => {
  * socket 事件
  */
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
+  console.log('new user connected');
   /**
    * Room
    */
   socket.on('join', (data) => {
     var obj = JSON.parse(data)
-    console.log("joined: " + obj.userID)
+    console.log("join => userID: " + obj.userID)
+    userGroup.push(obj.userID)
     socket.join(room)
-    if (!joinRoom.find ( it => it == obj.userID)) joinRoom.push(obj.userID)
-    if (joinRoom.length == 2) {
+
+    if (userGroup.find( it => it == "2AB0BCAC0AF7B35AD957540E491256498F31FC20") && userGroup.find( it => it == "8A88208FCE4862B7541F74D4EADBAAB71F6CBEBE")) {
+      var res = {roomID: "no1", userA: "2AB0BCAC0AF7B35AD957540E491256498F31FC20", userB: "8A88208FCE4862B7541F74D4EADBAAB71F6CBEBE", videoID: "https://www.youtube.com/watch?v=cCOMPI8doc0"}
+      io.emit('matched', res)
+    }
+  })
+
+  socket.on('room_isReady', (data) => {
+    var obj = JSON.parse(data)
+    console.log("room_isReady => userID: " + obj.userID)
+    if (!isReady.find ( it => it == obj.userID)) isReady.push(obj.userID)
+    if (isReady.length == 2) {
+      isReady.length = 0
+      currentQuestionIndex = -1
       gameStart()
-      isGameLoadingFinish = true
     }
   })
 
   socket.on('room_finish', (data) => {
     var obj = JSON.parse(data)
-    console.log("room_finish: " + obj.userID)
+    console.log("room_finish => userID: " + obj.userID)
     finishID.push({userID: obj.userID, useTime: obj.useTime})
     if (finishID.length == 2) judge()
   })
 
 
   socket.on('leave', (data) => {
-    isGameLoadingFinish = false
     var obj = JSON.parse(data)
+    console.log("leave => userID: " + obj.userID)
     socket.leave(room)
-    userGroup
+    socket.disconnect()
     var index = userGroup.indexOf(obj.userID)
     if (index != -1) userGroup.splice(index, 1)
   })
@@ -94,18 +97,18 @@ server.listen(8082, () => {
  */
 
 function gameStart() {
-  console.log("gameStart")
-  clearTimeout(timeoutID)
   currentQuestionIndex ++
   io.in(room).emit('room_game_start', {index: currentQuestionIndex})
+  clearTimeout(timeoutID)
   timeoutID = setTimeout(timeOut, 60000)
+  console.log("gameStart: " + currentQuestionIndex)
 }
 
 function judge() {
   if (finishID.length == 2) {
-    if (finishID[0].useTime > finishID[1].useTime) {
+    if (finishID[0].useTime < finishID[1].useTime) {
       io.in(room).emit('room_judge', {userID: finishID[0].userID})
-    } else if (finishID[0].useTime < finishID[1].useTime) {
+    } else if (finishID[0].useTime > finishID[1].useTime) {
       io.in(room).emit('room_judge', {userID: finishID[1].userID})
     } else {
       io.in(room).emit('room_judge', {userID: ""})
@@ -115,6 +118,7 @@ function judge() {
   } else {
     io.in(room).emit('room_judge', {userID: ""})
   }
+  clearTimeout(timeoutID)
   timeoutID = setTimeout(gameStart, 2000)
   finishID.length = 0
 }
